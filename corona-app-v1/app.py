f348fcca6a4ea8da80f92701ec7b6240ab58c5ab
Meinhard Ploner <meinhard.ploner@gmail.com>
2020-03-17 18:33:04 +0100

    Updates and refined styles.

--- corona-app-v1/app.py
+++ corona-app-v1/app.py
@@ -7,55 +7,62 @@ import pandas as pd
 
 baseURL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/"
 
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+tickFont = {'size':12, 'color':"rgb(30,30,30)", 'family':"Courier New, monospace"}
+
 def loadData(fileName, columnName): 
-    allData = pd.read_csv(baseURL + fileName) \
-                .drop(['Lat', 'Long'], axis=1) \
-                .melt(id_vars=['Province/State', 'Country/Region'], var_name='date', value_name=columnName) \
-                .fillna('<all>')
-    allData['date'] = allData['date'].astype('datetime64[ns]')
-    return allData
+    data = pd.read_csv(baseURL + fileName) \
+             .drop(['Lat', 'Long'], axis=1) \
+             .melt(id_vars=['Province/State', 'Country/Region'], var_name='date', value_name=columnName) \
+             .fillna('<all>')
+    data['date'] = data['date'].astype('datetime64[ns]')
+    return data
 
-allData = loadData("time_series_19-covid-Confirmed.csv", "Confirmed") \
-    .merge(loadData("time_series_19-covid-Deaths.csv", "Deaths")) \
-    .merge(loadData("time_series_19-covid-Recovered.csv", "Recovered"))
+allData = loadData("time_series_19-covid-Confirmed.csv", "CumConfirmed") \
+    .merge(loadData("time_series_19-covid-Deaths.csv", "CumDeaths")) \
+    .merge(loadData("time_series_19-covid-Recovered.csv", "CumRecovered"))
 
-app = dash.Dash(__name__)
+countries = allData['Country/Region'].unique()
+countries.sort()
 
-app.layout = html.Div(children=[
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div(
+    style={ 'font-family':"Courier New, monospace" },
+    children=[
     html.H1('Case History of the Coronavirus (COVID-19)'),
     html.Div(className="row", children=[
         html.Div(className="four columns", children=[
             html.H5('Country'),
             dcc.Dropdown(
                 id='country',
-                options=[{'label': state, 'value': state} for state in ['Italy', 'US', 'Australia', 'China']],
+                options=[{'label':c, 'value':c} for c in countries],
                 value='Italy'
-            ),
+            )
         ]),
         html.Div(className="four columns", children=[
             html.H5('State / Province'),
             dcc.Dropdown(
                 id='state'
-            ),
+            )
         ]),
         html.Div(className="four columns", children=[
             html.H5('Selected Metrics'),
             dcc.Checklist(
                 id='metrics',
-                options = [
-                    {'label': 'Confirmed', 'value': 'Confirmed'},
-                    {'label': 'Deaths', 'value': 'Deaths'},
-                    {'label': 'Recovered', 'value': 'Recovered'},
-                ],
-                value = ['Confirmed', 'Deaths']            
+                options=[{'label':m, 'value':m} for m in ['Confirmed', 'Deaths', 'Recovered']],
+                value=['Confirmed', 'Deaths']
             )
         ])
     ]),
     dcc.Graph(
-        id="plot_new_metrics"
+        id="plot_new_metrics",
+        config={ 'displayModeBar': False }
     ),
     dcc.Graph(
-        id="plot_cum_metrics"
+        id="plot_cum_metrics",
+        config={ 'displayModeBar': False }
     )
 ])
 
@@ -77,13 +84,29 @@ def nonreactive_data(country, state):
         data = data.drop('Province/State', axis=1).groupby("date").sum().reset_index()
     else:
         data = data.loc[data['Province/State'] == state]
-    data = data.join(data.select_dtypes(include='int64').diff().add_prefix('New').fillna(0))
+    newCases = data.select_dtypes(include='int64').diff().fillna(0)
+    newCases.columns = [column.replace('Cum', 'New') for column in newCases.columns]
+    data = data.join(newCases)
+    data['dateStr'] = data['date'].dt.strftime('%b %d, %Y')
     return data
 
-def barchart(data, metrics, metricPrefix=""):
-    return go.Figure(data=[
-        go.Bar(name=metric, x=data.date, y=data[metricPrefix + metric]) for metric in metrics
-    ]).update_layout(barmode='group')
+def barchart(data, metrics, prefix="", yaxisTitle=""):
+    figure = go.Figure(data=[
+        go.Bar( 
+            name=metric, x=data.date, y=data[prefix + metric],
+            marker_line_color='rgb(0,0,0)', marker_line_width=1,
+            marker_color={ 'Deaths':'rgb(200,30,30)', 'Recovered':'rgb(30,200,30)', 'Confirmed':'rgb(100,140,240)'}[metric]
+        ) for metric in metrics
+    ])
+    figure.update_layout( 
+              barmode='group', legend=dict(x=.05, y=0.95, font={'size':15}, bgcolor='rgba(240,240,240,0.5)'), 
+              plot_bgcolor='#FFFFFF', font=tickFont) \
+          .update_xaxes( 
+              title="", tickangle=-90, type='category', showgrid=True, gridcolor='#DDDDDD', 
+              tickfont=tickFont, ticktext=data.dateStr, tickvals=data.date) \
+          .update_yaxes(
+              title=yaxisTitle, showgrid=True, gridcolor='#DDDDDD')
+    return figure
 
 @app.callback(
     Output('plot_new_metrics', 'figure'), 
@@ -91,7 +114,7 @@ def barchart(data, metrics, metricPrefix=""):
 )
 def update_plot_new_metrics(country, state, metrics):
     data = nonreactive_data(country, state)
-    return barchart(data, metrics, metricPrefix="New")
+    return barchart(data, metrics, prefix="New", yaxisTitle="New Cases per Day")
 
 @app.callback(
     Output('plot_cum_metrics', 'figure'), 
@@ -99,9 +122,7 @@ def update_plot_new_metrics(country, state, metrics):
 )
 def update_plot_cum_metrics(country, state, metrics):
     data = nonreactive_data(country, state)
-    return barchart(data, metrics, metricPrefix="")
+    return barchart(data, metrics, prefix="Cum", yaxisTitle="Cumulated Cases")
 
 if __name__ == '__main__':
     app.run_server(debug=True)
-
-    
